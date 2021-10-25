094aa1ee708b35741e9b5d5ede8e74e50da8e138
Mariano Guisado Porres <marianodosogui@gmail.com>
2021-10-25 13:55:41 +0200

    primer commit

---
+++ src/main/java/com/everis/proyectoGit/App.java
@@ -0,0 +1,13 @@
+package com.everis.proyectoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hola Mariano Guisado Porres" );
+    }
+}
